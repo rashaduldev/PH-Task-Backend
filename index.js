1b519d0d67447010f092889dd78e063656dc3e44
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-12-22 17:35:34 +0600

    task collection added

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port=process.env.PORT || 3000
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://phtask:IZHDq87C4pNkMfmQ@cluster0.ioitfil.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const usercollection = client.db("Task").collection("users");
+    const alltaskCollection = client.db("Task").collection("task");
+
+       // get user to Display
+       app.get('/users', async(req,res)=>{
+        console.log(req.headers);
+        const result=await alltaskCollection.find().toArray();
+        res.send(result);
+      })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
