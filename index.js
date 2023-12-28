b8d83e656100af88750c2178e03c26f37b975662
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-12-28 15:52:54 +0600

    User get and task get Done

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const port=process.env.PORT || 3000
 // middleware
 app.use(cors());
 app.use(express.json());
+// username: mdrashadul898
+// password: AYrD9M1mFDQuQbxT 
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://phtask:IZHDq87C4pNkMfmQ@cluster0.ioitfil.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://mdrashadul898:AYrD9M1mFDQuQbxT@cluster0.hq3qxum.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,23 +26,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const usercollection = client.db("AllTask").collection("users");
+    const taskscollection = client.db("AllTask").collection("tasks");
 
-    const usercollection = client.db("Task").collection("users");
-    const alltaskCollection = client.db("Task").collection("task");
+    // get user information 
+    app.get('/users', async(req,res)=>{
+      const result=await usercollection.find().toArray();
+      res.send(result);
+    })
+    // get task information 
+   app.get('/tasks',async(req,res)=>{
+    const result=await taskscollection.find().toArray();
+    res.send(result);
+   })
 
-       // get user to Display
-       app.get('/task', async(req,res)=>{
-        console.log(req.headers);
-        const result=await alltaskCollection.find().toArray();
-        res.send(result);
-      })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
